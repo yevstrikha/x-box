a589034fc4ef85889503c2cbbb2e29c7a4f48eb9
yevstrikha <94506191+yevstrikha@users.noreply.github.com>
2022-01-14 22:18:16 -0500

    new section

--- js/script.js
+++ js/script.js
@@ -17,3 +17,41 @@ $(document).ready(function(){
         }
     });
 });
+$('.owl-value').owlCarousel({
+    loop:true,
+    margin:30,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            margin:0,
+            nav:true
+        },
+        1000:{
+            items:4,
+            margin:40,
+            nav:true,
+            loop:true
+        }
+    }
+})
+// $('.owl-value').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:4
+//         }
+//     }
+// })
